f616ac1271b2e611324bfd97d7542ac194d078a1
Arjunxxxl <a.asankhala@gmail.com>
2018-09-28 17:16:48 +0530

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Controller : MonoBehaviour {
+
+	public float jumpForce = 400f;
+	public float crouchSpeed = .36f;
+	public float moveSmooth = 0.05f;
+	public bool airControl = false;
+	public LayerMask whatIsGround;
+	public Transform ceil;
+	public Transform floor;
+	public Collider2D disableCollider;
+
+	float GroundedRadius = 0.2f;
+	public bool grounded;
+	float ceilRadius = 0.2f;
+	Rigidbody2D rb;
+	bool facingRight = true;
+	Vector3 velocity = Vector3.zero;
+
+	public int jumpCount = 1;
+
+	[Header("Events")]
+	[Space]
+
+	public UnityEvent onLandEvent;
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool>{ }
+
+	public BoolEvent OnCrouchevent;
+	bool crouching = false;
+
+	public static Controller Instance;
+
+	// Use this for initialization
+	void Awake () {
+		Instance = this;
+
+
+		rb = GetComponent<Rigidbody2D>();
+
+		if(onLandEvent == null)
+		{
+			onLandEvent = new UnityEvent();
+		}
+		if(OnCrouchevent == null)
+		{
+			OnCrouchevent = new BoolEvent();
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		bool wasGrounded = grounded;
+		grounded = false;
+
+		//Debug.Log(Physics2D.OverlapCircle(ceil.position, ceilRadius, whatIsGround));
+
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(floor.position, GroundedRadius, whatIsGround);
+		for(int i = 0; i<colliders.Length; i++)
+		{
+			if(colliders[i].gameObject != gameObject)
+			{
+				grounded = true;
+				if(!wasGrounded)
+				{
+					onLandEvent.Invoke();
+				}
+			}
+		}
+
+		if(grounded)
+		{
+			jumpCount = 1;
+		}
+	}
+
+	public void Move(float move, bool crouch, bool jump, bool secondJump)
+	{
+		if(!crouch)
+		{
+			if(Physics2D.OverlapCircle(ceil.position, ceilRadius, whatIsGround))
+			{
+				crouch = true;
+			}
+		}
+
+		if(grounded || airControl)
+		{
+			if(crouch)
+			{
+				if(!crouching)
+				{
+					crouching = true;
+					OnCrouchevent.Invoke(true);
+				}
+
+				move *= crouchSpeed;
+
+				if(disableCollider!=null)
+				{
+					disableCollider.enabled = false;
+				}
+			}
+			else
+			{
+				if(disableCollider!=null)
+				{
+					disableCollider.enabled = true;
+				}
+
+				if(crouching)
+				{
+					crouching = false;
+					OnCrouchevent.Invoke(false);
+				}
+			}
+
+			Vector3 targetVelocity = new Vector2(move*10f, rb.velocity.y);
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, moveSmooth);
+
+
+			if(move>0 && !facingRight)
+			{
+				Flip();
+			}
+			else if(move<0 && facingRight)
+			{
+				Flip();
+			}
+		}
+
+		if(grounded && jump)
+		{
+			grounded = false;
+			rb.AddForce(new Vector2(0, jumpForce));
+		}
+
+		if(!grounded && secondJump && jumpCount>0)
+		{
+			grounded = false;
+			rb.AddForce(new Vector2(0, jumpForce*.8f));
+			jumpCount--;
+		}
+
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 localS = transform.localScale;
+		localS.x *= -1;
+		transform.localScale = localS;
+	}
+
+}
---
+++ PlayerMotor.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+	public Controller contol;
+	float h;
+
+	public float runSpeed = 400f;
+	public bool jump;
+	public bool secondJump;
+	public int jumpCount = 0;
+	public bool crouch;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+		{	
+			if(jump || secondJump)
+			{
+				return;
+			}
+
+			jump = true;
+			anim.SetBool("jump", true);
+
+			
+			if(jumpCount==1)
+			{
+				secondJump = true;
+				anim.SetBool("jump", false);
+				anim.SetTrigger("second");
+			}
+
+			jumpCount++;
+			Debug.Log(secondJump);
+		}
+
+
+		if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.Keypad0))
+		{
+			crouch = true;
+			anim.SetBool("crouch", true);
+		}
+
+		if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			jump = false;
+		}
+
+		if(Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.Keypad0))
+		{
+			crouch = false;
+		}
+
+		h = Input.GetAxisRaw("Horizontal");
+
+		anim.SetFloat("Speed", Mathf.Abs(h));
+
+	}
+
+	private void FixedUpdate() {
+		contol.Move(h, crouch, jump, secondJump);
+		if(jump)
+		{jump = false; }
+		
+	}
+
+	public void OnCrouchEvent(bool b)
+	{
+		anim.SetBool("crouch", b);
+	}
+
+	public void OnLandEvent()
+	{
+		//anim.SetBool("jump", false);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if(other.gameObject.tag == "ground")
+		{
+			anim.SetBool("jump", false);
+			secondJump = false;
+			jumpCount = 0;
+		}
+	}
+
+}
